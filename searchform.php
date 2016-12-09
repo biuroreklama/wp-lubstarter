d1a0ea47307007eba99e1177d9aa35dc8966d276
Biuro "Rynek Lubelski <biuro@ryneklubelski.pl>
2016-12-09 15:13:03 +0100

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
-		<span class="screen-reader-text"><?php echo _x( '', 'label', 'arkadia' ); ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Szukaj &hellip;', 'placeholder', 'arkadia' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+		<span class="screen-reader-text"><?php echo _x( '', 'label', 'lubstarter' ); ?></span>
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Szukaj &hellip;', 'placeholder', 'lubstarter' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
 	</label>
-	<button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo _x( '<i class="fa fa-search"></i>', 'submit button', 'arkadia' ); ?></span></button>
+	<button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo _x( '<i class="fa fa-search"></i>', 'submit button', 'lubstarter' ); ?></span></button>
 </form>
